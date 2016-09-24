a36041dd8253119d2aeb86d5464ea644240a5c6d
勝算はどんな時でもある なければ作るだけだ。Endopasmic < Endopasmic>
2016-09-24 16:18:03 +0900

    improve code

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,40 +8,42 @@
  * to execute feature. It should mainly call function
  * not coding algorithm.
  */
+(function() {
+  io.socket.on('connect', function () {// if web socket online
 
-io.socket.on('connect', function() {// if web socket online
+    //wait for broadcast message
+    io.socket.on('message', function (data) {
+      console.log("broadcast: " + data.msg);
 
-  //wait for broadcast message
-  io.socket.on('message', function (data){
-    console.log("broadcast: "+data.msg);
-
-    $(".chat").append(
-      //text is detail that user input
-      '<p class="them"> ' +data.msg + '</p>'
-    );
-
-    $.get('/Chat/TalkSession',{msg: data.msg},function (data){
-
+      //text will popup detail that user input
       $(".chat").append(
-        //text is detail that bot response
-        '<p class="me"> ' +data.answer + '</p>'
+        '<p class="them"> ' + data.msg + '</p>'
       );
 
+      $.get('/Chat/TalkSession', {msg: data.msg}, function (data) {
+
+        $(".chat").append(
+          //text is detail that bot response
+          '<p class="me"> ' + data.answer + '</p>'
+        );
+
+      });
     });
+
+    //make new room when page load
+    NewRoom();
+
+    //request to broadcast to room when send messages.
+    $('#chat-send-button').click(SendMessage);
+
+    // When the socket disconnects
+    io.socket.on('disconnect', function () {
+
+      console.log("socket disconnect");
+
+    });
+
   });
 
-  //make new room when page load
-  NewRoom();
-
-  //request to broadcast to room when send messages.
-  $('#chat-send-button').click(SendMessage);
-
-  // When the socket disconnects
-  io.socket.on('disconnect', function() {
-
-    console.log("socket disconnect");
-
-  });
-
-});
+})();
 
